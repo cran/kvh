1eb142d0694ed630554bce14b31d5497edf776e4
Serguei Sokol <sokol@insa-toulouse.fr>
2018-06-15 12:43:36 +0000

    version 1.3

--- src/rcpp_kvh.cpp
+++ src/rcpp_kvh.cpp
@@ -36,7 +36,7 @@ bool indent_lacking(string& buf, size_t& lev) {
     if (lev == 0)
         return false; // at level 0 no lacking tabs
     if (buf.size() < lev)
-        return true; // too short to have sufficient number of bits
+        return true; // too short to have sufficient number of tabs
     for (size_t i=0; i < lev; i++) {
         if (buf[i] != '\t')
             return true;
@@ -45,6 +45,7 @@ bool indent_lacking(string& buf, size_t& lev) {
 }
 bool escaped_eol(string& buf) {
     // test if end_of_line is escaped or not in buf
+//Rcout << "escaped_eol: testing '" << buf << "'\n";
     int i;
     if (buf.size() == 0)
         return false;
@@ -52,9 +53,30 @@ bool escaped_eol(string& buf) {
         ; // find out the position of the last backslash series
     }
     i=buf.size()-i-1;
+//Rcout << "result=" << (bool) i%2 << "\n";
     return i%2;
 }
-string kvh_get_line(ifstream& fin, size_t* ln, const string& comment_str, const bool strip_white) {
+inline void strip_wh(string &s) {
+//Rcout << "strip_wh: stripping '" << s << "'\n";
+    if (s.size() == 0)
+        return;
+    size_t pstr;
+    pstr=s.find_first_not_of(whitespaces);
+    if (pstr != string::npos) {
+        s.erase(0, pstr);
+//Rcout << "strip_wh: striped left '" << s << "'\n";
+    } else {
+        s.clear(); // s is all whitespace
+        return;
+    }
+    pstr=s.find_last_not_of(whitespaces);
+    if (pstr != string::npos)
+        s.erase(pstr+1);
+//Rcout << "strip_wh: striped right '" << s << "'\n";
+    return;
+}
+
+string kvh_get_line(ifstream& fin, size_t* ln, const string& comment_str) {
     // get a string from stream that ends without escaped eol character and increment ln[0]
     string b, res;
     size_t pstr;
@@ -71,19 +93,13 @@ string kvh_get_line(ifstream& fin, size_t* ln, const string& comment_str, const 
         if (pstr != string::npos && (b=res.substr(0, pstr), true) && !escaped_eol(b)) // stip out non escaped comments
             res=b;
     }
-    if (strip_white) {
-        pstr=res.find_last_not_of(whitespaces);
-        if (pstr != string::npos)
-            res.erase(pstr+1);
-        else
-            res.clear(); // res is all whitespace
-    }
     return res;
 }
-keyval kvh_parse_kv(string& line, size_t& lev) {
+keyval kvh_parse_kv(string& line, size_t& lev, const bool strip_white, const string &split_str) {
     // get key-value pair from the line
     keyval kv;
     size_t i, bs; // count backslashes;
+    string s;
 //Rcout << "lev=" << lev << ";\tline=" << line << endl;
     for (i=lev, bs=0; i < line.size(); i++) {
         if (line[i] == '\\') {
@@ -93,7 +109,39 @@ keyval kvh_parse_kv(string& line, size_t& lev) {
         if (line[i] == '\t' && (bs+1)%2) {
 //Rcout << "line[" << i << "]=" << line[i] << "; bs=" << bs << endl;
             kv.key=unescape(line.substr(lev, i-lev));
-            kv.val=unescape(line.substr(i+1));
+            if (strip_white) {
+                strip_wh(kv.key);
+            }
+            s=line.substr(i+1);
+            if (split_str.size() > 0) {
+                // split s
+                vector<string> vs; // will have results of split
+                size_t pos, splpos; // position of the split string in s
+                string subs;
+                for (pos=0; pos < s.size() && (splpos=s.find(split_str, pos)) != string::npos;) {
+                    for (subs=s.substr(pos, splpos-pos); escaped_eol(subs) && splpos != string::npos; splpos=s.find(split_str,  splpos+split_str.size()), subs=s.substr(pos, splpos-pos))
+                        ; // find first unescaped splpos
+//Rcout << "pos=" << pos << "\n";
+                    if (strip_white)
+                        strip_wh(subs);
+                    vs.push_back(unescape(subs));
+                    pos=splpos+split_str.size();
+//Rcout << "new pos=" << pos << "\n";
+                }
+//Rcout << "last pos=" << pos << "\n";
+                subs=s.substr(pos);
+                if (strip_white)
+                    strip_wh(subs);
+                vs.push_back(unescape(subs)); // add the last field at the end of s
+                StringVector vs_r(vs.size());
+                for (size_t i=0; i < vs.size(); i++)
+                    vs_r[i]=vs[i];
+                kv.val=wrap(vs_r);
+            } else {
+                if (strip_white)
+                    strip_wh(s);
+                kv.val=unescape(s);
+            }
             kv.tab_found=true;
             break;
         }
@@ -102,12 +150,14 @@ keyval kvh_parse_kv(string& line, size_t& lev) {
     if (i == line.size()) {
         // no tab found => the whole string goes to the key
         kv.key=unescape(line.substr(lev));
+        if (strip_white) 
+            strip_wh(kv.key);
         kv.val="";
         kv.tab_found=false;
     }
     return(kv);
 }
-list_line kvh_read(ifstream& fin, size_t lev, size_t* ln, const string& comment_str, const bool strip_white, const bool skip_blank) {
+list_line kvh_read(ifstream& fin, size_t lev, size_t* ln, const string& comment_str, const bool strip_white, const bool skip_blank, const string& split_str, const bool follow_url) {
     // recursively read kvh file and return its content in a nested named list of character vectors
     List res=List::create() ;
     keyval kv;
@@ -119,11 +169,11 @@ list_line kvh_read(ifstream& fin, size_t lev, size_t* ln, const string& comment_
     while (!fin.eof()) { // && i++ < 5) {
         // get full line (i.e. concat lines with escaped end_of_line)
         if (read_stream)
-            line=kvh_get_line(fin, ln, comment_str, strip_white);
+            line=kvh_get_line(fin, ln, comment_str);
 //print(wrap(line));
 //print(wrap(fin.eof()));
-        if (skip_blank && (line.size() == 0 || (!strip_white && line.find_first_not_of(whitespaces) == string::npos)) && !fin.eof())
-            continue;
+        if (skip_blank && (line.size() == 0 || (strip_white && line.find_first_not_of(whitespaces) == string::npos)) && !fin.eof())
+            continue; // skip white line
         if ((line.size() == 0 && fin.eof()) || (lev && indent_lacking(line, lev))) {
             // current level is ended => go upper and let treat the line (already read) there
             res.attr("ln")=(int) ln[0];
@@ -132,16 +182,32 @@ list_line kvh_read(ifstream& fin, size_t lev, size_t* ln, const string& comment_
             ll.line=line;
             return ll;
         }
-        kv=kvh_parse_kv(line, lev);
+        kv=kvh_parse_kv(line, lev, strip_white, split_str);
         ln_save=ln[0];
 //print(wrap(List::create(_["l"]=line, _["k"]=kv.key, _["v"]=kv.val)));
         read_stream=kv.tab_found;
         if (!kv.tab_found) {
             // tab is absent => we have to go deeper in the hierarchy level
-            ll=kvh_read(fin, lev+1, ln, comment_str, strip_white, skip_blank);
+            ll=kvh_read(fin, lev+1, ln, comment_str, strip_white, skip_blank, split_str, follow_url);
             kv.val=(ll.res.size() == 0 ? "" : ll.res);
             line=ll.line;
         } // else simple key-value pair
+        if (follow_url && kv.val.sexp_type() == STRSXP) {
+//Rcout << "follow_url\n";
+            CharacterVector cval(kv.val);
+            if (cval.size() == 1) {
+                string sval=as<string>(cval[0]);
+                if (sval.substr(0, 7) == "file://") {
+                    sval=sval.substr(7);
+//Rcout << "trying to kvh_read '" << sval << "'\n";
+                    kv.val=kvh_read(sval, comment_str, strip_white, skip_blank, split_str, follow_url);
+                    if (kv.val.isNULL()) {
+//Rcout << "set kv.val back to file://...\n";
+                        kv.val=cval;
+                    }
+                }
+            }
+        } // else if (kv.val.sexp_type() == STRSXP) { CharacterVector cval(kv.val); string sval=as<string>(cval[0]); if (sval.substr(0, 7) == "file://") Rcout << "not following '" << sval << "'\n";}
         kv.val.attr("ln")=(int) ln_save;
         res.push_back(kv.val);
         nm.push_back(kv.key);
@@ -162,14 +228,26 @@ list_line kvh_read(ifstream& fin, size_t lev, size_t* ln, const string& comment_
 //'   string itself and everything following it on the line is ignored. Note that
 //'   lines are first appended if end lines are escaped and then a search for a
 //'   comment string is done.
-//' @param strip_white logical optional control of white spaces at the end of lines (default FALSE)
+//' @param strip_white logical optional control of white spaces on both ends of keys and values (default FALSE)
 //' @param skip_blank logical optional control of lines composed of only white characters after a possible stripping of a comment (default FALSE)
+//' @param split_str character optional string by which a value string can be splitted in several strings (default: empty string, i.e. no splitting)
+//' @param follow_url logical optional control of recursive kvh reading and parsing. If set to TRUE and a value starts with 'file://' then the path following this prefix will be passed as argument 'fn' to another 'kvh_read()' call. The list returned by this last call will be affected to the corresponding key instead of the value 'file://...'. If a circular reference to some file is detected, a warning is emmited and the faulty value 'file://...' will be left without change. The rest of the file is proceeded as usual.
 //' @export
 // [[Rcpp::export]]
-List kvh_read(const string& fn, const string& comment_str="", const bool strip_white=false, const bool skip_blank=false) {
+RObject kvh_read(string fn, const string& comment_str="", const bool strip_white=false, const bool skip_blank=false, const string& split_str="", const bool follow_url=false) {
     // read kvh file and return its content in a nested named list of character vectors
     if (comment_str.find('\t') < string::npos || comment_str.find('\n') < string::npos)
         stop("parameter 'comment_str' cannot have tabulation or new line characters");
+//Rcout << "follow_url=" << follow_url << "\n";
+    // check for nested references if follow_url=true
+    static set<string> read_files;
+    if (follow_url) {
+        if (read_files.find(fn) != read_files.end()) {
+            warning("kvh_read: detected circular reference to file '%s'", fn);
+            return R_NilValue;
+        }
+        read_files.insert(fn);
+    }
     // open file for binary reading
     ifstream fin;
     list_line ll;
@@ -177,7 +255,10 @@ List kvh_read(const string& fn, const string& comment_str="", const bool strip_w
     fin.open(fn.data(), ios::in | ios::binary);
     if (!fin)
         stop("cannot open file '%s' for reading", fn);
-    ll=kvh_read(fin, 0, &ln, comment_str, strip_white, skip_blank);
+    ll=kvh_read(fin, 0, &ln, comment_str, strip_white, skip_blank, split_str, follow_url);
     fin.close();
+    if (follow_url)
+        read_files.erase(fn);
+    ll.res.attr("file")=fn;
     return ll.res;
 }
--- tests/testthat/test_kvh.R
+++ tests/testthat/test_kvh.R
@@ -20,11 +20,57 @@ test_that("obj_by_keys", {
 wd=tempdir(TRUE)
 fn=tempfile(pattern = "file", tmpdir = wd, fileext = ".kvh")
 fc=file(fn, "wb")
-cat("a\tb \n", file=fc)
+cat(" a \t b \n", file=fc)
 close(fc)
 res=kvh::kvh_read(fn, strip_white=TRUE)
 test_that("strip_white",
-  expect_equal(res[["a"]], "b", info="strip space")
+  expect_equal(res[["a"]], "b", info="strip spaces")
+)
+fc=file(fn, "wb")
+cat("a\tb\n\n\t\t \r\nc\td\n", file=fc)
+close(fc)
+res=kvh::kvh_read(fn, skip_blank=TRUE)
+test_that("skip_blank", {
+  expect_equal(res[["a"]], "b", info="before skip blank")
+  expect_equal(res[["c"]], "d", info="after skip blank")
+  expect_equal(length(res), 2, info="total skip blank")
+}
 )
 
+# test commnets, stripping white and skipping blank
+fc=file(fn, "wb")
+cat("a\tb # comment\n\t# comment on a blank line\nc\td", file=fc)
+close(fc)
+res=kvh::kvh_read(fn, comment_str="#", strip_white=TRUE, skip_blank=TRUE)
+test_that("comment strip & skip_blank", {
+  expect_equal(res[["a"]], "b", info="before comment strip & skip blank")
+  expect_equal(res[["c"]], "d", info="after comment strip & skip blank")
+  expect_equal(length(res), 2, info="total comment strip & skip blank")
+}
+)
+
+# test spliting value string by tabs
+fc=file(fn, "wb")
+cat("a\tb1\tb2\\\tb2_too\tb3\n", file=fc)
+close(fc)
+res=kvh::kvh_read(fn, split_str="\t")
+test_that("split value", {
+  expect_equal(res[["a"]], c("b1", "b2\tb2_too", "b3"))
+}
+)
+
+# test follow_url and strip_white
+fn2=tempfile(pattern = "file", tmpdir = wd, fileext = ".kvh")
+fc=file(fn, "wb")
+cat("a\t file://", fn2, " \n", sep="", file=fc)
+close(fc)
+fc=file(fn2, "wb")
+cat("k\tv\nbad nested ref\t file://", fn, " \n", sep="", file=fc)
+close(fc)
+res=kvh::kvh_read(fn, strip_white=TRUE, follow_url=TRUE)
+test_that("split value", {
+  expect_equal(res[["a"]], list(k="v", `bad nested ref`=paste0("file://", fn)), check.attributes=FALSE)
+  expect_warning(kvh::kvh_read(fn, strip_white=TRUE, follow_url=TRUE), "kvh_read: detected nested reference to file ")
+}
+)
 unlink(fn)
